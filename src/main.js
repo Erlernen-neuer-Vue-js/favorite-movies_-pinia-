7aa3c380328cf864e7b3a5db6c7ee0afffa0142c
Verda Muso <gearmobile@gmail.com>
2024-04-30 21:19:08 +0300

    import pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia' // <- 1
 import App from './App.vue'
 import './main.css'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(createPinia()) // <- 2
+    .mount('#app')
